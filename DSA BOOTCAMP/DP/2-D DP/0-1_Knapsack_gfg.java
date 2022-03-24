3264c502ae76247c0ce4e17d11ca18cc35d2eeb2
Ishaan Thakral <thakralishaan@gmail.com>
2022-03-24 16:23:29 +0530

    Bootcamp

---
+++ DSA BOOTCAMP/DP/2-D DP/0-1_Knapsack_gfg.java
@@ -0,0 +1,29 @@
+public static int knapSack(int Capacity, int wt[], int val[], int n) 
+    { 
+         return maxProfit(Capacity, wt, val,0,n, new HashMap<>());
+    } 
+    
+    public static int maxProfit(int capacity , int[] weight, int[] profit,  int currIndex,int n, HashMap<String, Integer> map)
+    {
+        if(currIndex>=n) return 0;
+
+         int currItemWeight =  weight[currIndex];
+         int currItemProfit =  profit[currIndex];
+                                        
+        String key = currIndex + "-" + capacity;
+        
+        if(map.containsKey(key))
+            return map.get(key);
+
+        int pickItem = 0;                             //as if and else statement both can take its value so we specify 
+        
+        if(currItemWeight<=capacity)                 //checking if we can even add the item to the bag if yes then
+        pickItem = currItemProfit + maxProfit(capacity - currItemWeight, weight, profit,currIndex+1, n, map);   //decrease the capacity by the weight of the item and go to next index
+        
+        int nonPickItem =  maxProfit(capacity , weight, profit,currIndex+1,  n, map);
+        
+         map.put(key, Math.max(pickItem, nonPickItem));                // T.C 0(n)  S.C 0(n)
+        
+        return map.get(key);
+        
+    }
